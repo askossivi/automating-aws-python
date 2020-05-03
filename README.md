e05c66fcc3329f5626a96f8ba3bd5c811f0856b2
askossivi <askossivi@mail.mil>
2020-05-03 18:26:40 -0400

    The usage of click to organize project script

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# Automating AWS with Python
+## Automating AWS with Python
 Repository for ASKOSSIVI Project *Automating AWS with  Python*
 
 ##project-01
 Project-01 is a script that will sync our local directory to an s3 bucket, and optionally configure Rout53 and cloudfront as well as s3 static webpage
+
+
+## Features
+Project-01 currently has the following features:
+
+- List bucket
+- List contents of a bucket
---
+++ project-01/project/project.py
@@ -0,0 +1,31 @@
+import boto3
+import click
+
+session = boto3.Session(profile_name='pythonAutomation')
+s3 = session.resource('s3')
+
+@click.group()
+def cli():
+    "Project deploys website to AWS"
+pass
+
+
+@cli.command('list-buckets')
+def list_buckets():
+    "List all s3 buckets"
+#pass == Place holder
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    "List object in se bucket"
+#pass == Place holder
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
+
+
+if __name__ == '__main__':
+    cli()
